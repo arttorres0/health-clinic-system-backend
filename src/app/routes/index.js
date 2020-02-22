b1913f6e91e3bf066ded8e08c7655239785b99ed
Arthur Torres <arttorres2004@hotmail.com>
2020-02-22 17:19:53 -0400

    Added Recepcionista and Paciente routes to routes/index.js

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,5 +2,7 @@ const express = require("express");
 const routes = express.Router();
 
 require("./medicos")(routes);
+require("./recepcionistas")(routes);
+require("./pacientes")(routes);
 
 module.exports = routes;
